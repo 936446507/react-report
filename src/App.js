55484b469112365130a9b5a64a88f512cc704473
arima <936446507@qq.com>
2018-09-25 22:01:36 +0800

    使用路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Home from './page/home/index'
+import Login from './page/login/index'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="app">
+          <Route path="/" component={Home} exact />
+          <Route path="/login" component={Login} exact />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default App
